755b14cd0e403012e298d5021565f7029796ede5
paulhenri-l <25308170+paulhenri-l@users.noreply.github.com>
2021-10-06 10:42:41 +0200

    Trust headers

--- src/LaravelLambdaEngineServiceProvider.php
+++ src/LaravelLambdaEngineServiceProvider.php
@@ -4,6 +4,7 @@ namespace PaulhenriL\LaravelLambdaEngine;
 
 use Illuminate\Support\Facades\Config;
 use PaulhenriL\LaravelEngineCore\EngineServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
 
 class LaravelLambdaEngineServiceProvider extends EngineServiceProvider
 {
@@ -16,6 +17,10 @@ class LaravelLambdaEngineServiceProvider extends EngineServiceProvider
     {
         Config::set('trustedproxy.proxies', Config::get('trustedproxy.proxies', [
             '127.0.0.1', '0.0.0.0/0', '2000:0:0:0:0:0:0:0/3'
+        ]));
+
+        Config::set('trustedproxy.headers', Config::get('trustedproxy.headers', [
+            Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO
         ]));
     }
 }
